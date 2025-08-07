d8bf3725f84680a75bcbd23ac007da999b08fdea
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-07 22:28:54 +0200

    Add YamlMurmelException class for handling YAML-related errors

--- src/main/java/de/murmelmeister/library/exceptions/YamlMurmelException.java
+++ src/main/java/de/murmelmeister/library/exceptions/YamlMurmelException.java
@@ -1,14 +1,34 @@
 package de.murmelmeister.library.exceptions;
 
+/**
+ * This class represents an exception thrown when there is an error related to YAML operations in the Murmel library.
+ * It extends {@link RuntimeException} to allow for unchecked exceptions that can be thrown during runtime.
+ */
 public class YamlMurmelException extends RuntimeException {
+    /**
+     * Constructs a new YamlMurmelException with the specified detail message.
+     *
+     * @param message The detail message for this exception
+     */
     public YamlMurmelException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new YamlMurmelException with the specified detail message and cause.
+     *
+     * @param message The detail message for this exception.
+     * @param cause   The cause of the exception, which can be retrieved later via the {@link Throwable#getCause()} method.
+     */
     public YamlMurmelException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a new YamlMurmelException with the specified cause.
+     *
+     * @param cause The cause of the exception, which can be retrieved later via the {@link Throwable#getCause()} method.
+     */
     public YamlMurmelException(Throwable cause) {
         super(cause);
     }
